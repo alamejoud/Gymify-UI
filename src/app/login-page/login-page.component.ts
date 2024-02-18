94f942263daf9d6c45145654c589941794211a07
alamejoud <alamejoud@gmail.com>
2024-02-18 12:29:59 +0200

    fixing logging in as signing up (by mistake)

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -15,7 +15,7 @@ export class LoginPageComponent {
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required && Validators.minLength(8)]
     });
   }
 
@@ -24,5 +24,25 @@ export class LoginPageComponent {
       console.log('Form Submitted:', this.loginForm.value);
       // Add your login logic here
     }
+  }
+
+  checkPasswordSize(): boolean {
+    return this.loginForm.get('password').value.length < 8;
+  }
+
+  checkExistingNumber(): boolean {
+    return this.loginForm.get('password').value.match(/\d+/g) === null;
+  }
+
+  checkExistingSpecialCharacter(): boolean {
+    return this.loginForm.get('password').value.match(/[^a-zA-Z0-9]+/g) === null;
+  }
+
+  checkExistingUpperCase(): boolean {
+    return this.loginForm.get('password').value.match(/[A-Z]+/g) === null;
+  }
+
+  validatePassword(): boolean {
+    return this.checkPasswordSize() || this.checkExistingNumber() || this.checkExistingSpecialCharacter() || this.checkExistingUpperCase();
   }
 }
